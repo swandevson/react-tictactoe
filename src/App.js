68ddbe7f79a49af7239527ee12864d12cac38ee0
gib.son <shai4956@gmail.com>
2024-07-15 18:10:30 +0900

    ✨ 부모 컴포넌트가 state를 관리하도록 변경

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={() => setValue("X")}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squareValues, setSquareValues] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squareValues.slice();
+    nextSquares[i] = "X";
+    setSquareValues(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squareValues[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squareValues[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squareValues[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squareValues[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squareValues[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squareValues[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squareValues[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squareValues[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squareValues[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
